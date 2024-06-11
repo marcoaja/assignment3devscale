0caa9666d043b55946ff0cdcd4cd84218dca2a8c
marco <ymarcogracia@gmail.com>
2024-06-11 16:33:47 +0700

    add all feature and add image

--- src/script.js
+++ src/script.js
@@ -6,3 +6,28 @@ const typed = new Typed("#typed", {
   backSpeed: 50,
   loop: true,
 });
+
+const active = [
+  "max-w-fit",
+  "gap-x-48",
+  "rounded-full",
+  "mt-10",
+  "bg-primary-500/70",
+  "shadow-lg",
+  "backdrop-blur-xl",
+  "text-primary-800",
+  "transition-all",
+  "duration-1000",
+];
+const navbar = document.getElementById("navbar");
+window.onscroll = () => {
+  if (window.scrollY > 0) {
+    active.forEach((cls) => {
+      navbar.classList.add(cls);
+    });
+  } else {
+    active.forEach((cls) => {
+      navbar.classList.remove(cls);
+    });
+  }
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,25 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: "16px",
+      },
       colors: {
         primary: { ...colors.indigo, DEFAULT: colors.indigo[600] },
       },
       fontFamily: {
         custom: ["Poppins", "sans-serif"],
+      },
+      animation: {
+        "loop-scroll": "loop-scroll 50s linear infinite",
+      },
+
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
       },
     },
   },
